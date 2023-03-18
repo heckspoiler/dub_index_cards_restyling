ad8626718d29096ec5ee98fe40d099c856c6e91c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-18 21:19:06 +0100

    adding functionality to the index card choice field

--- card_editor/js_files/index_card_logic.js
+++ card_editor/js_files/index_card_logic.js
@@ -3,3 +3,48 @@
 // learn button section
 
 // delete button section
+
+// add index card type pop up
+
+const buttonLeft = document.querySelector(".single-answer");
+const buttonRight = document.querySelector(".multiple-choice-answer");
+const buttonTop = document.querySelector(".open-format-answer");
+const boxLeft = document.querySelector(".left");
+const boxRight = document.querySelector(".right");
+const boxTop = document.querySelector(".top");
+const text = document.querySelector(".text");
+
+buttonLeft.addEventListener("click", () => {
+  if (boxLeft.style.marginLeft === "100%") {
+    boxLeft.style.marginLeft = "0%";
+    boxLeft.style.opacity = 1;
+    text.style.marginLeft = "-1000px";
+  } else {
+    boxLeft.style.marginLeft = "100%";
+    text.style.marginLeft = "0px";
+  }
+});
+
+buttonRight.addEventListener("click", () => {
+  if (boxRight.style.marginRight === "100%") {
+    boxRight.style.marginRight = "0%";
+    boxRight.style.opacity = 1;
+    text.style.marginLeft = "1200px";
+  } else {
+    boxRight.style.marginRight = "100%";
+    text.style.marginLeft = "0px";
+  }
+});
+
+buttonTop.addEventListener("click", () => {
+  if (boxTop.style.marginTop === "100%") {
+    boxTop.style.marginTop = "0%";
+    boxTop.style.opacity = 1;
+    text.style.marginTop = "-10rem";
+  } else {
+    boxTop.style.marginTop = "100%";
+    text.style.marginTop = " 10rem";
+  }
+
+  console.log(buttonTop);
+});
